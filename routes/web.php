f5fe2334285c01afd41befb8d646297bc275cdb2
Alex Groat <alex.groat.95@gmail.com>
2022-05-17 16:06:28 +0930

    added all routes to project

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,18 @@
 <?php
 
+use App\Http\Controllers\CartController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/products', [ProductController::class, 'productList'])->name('products.list');
+
+Route::controller(CartController::class)->group(function () {
+    Route::get('cart', 'cartList')->name('cart.list');
+    Route::post('cart', 'addToCart')->name('cart.store');
+    Route::post('update-cart', 'updateCart')->name('cart.update');
+    Route::post('remove', 'removeCart')->name('cart.remove');
+    Route::post('clear', 'clearAllCart')->name('cart.clear');
 });
